9b8776588dbc3710caff4952ee059c44b5e9cba5
Phyrex Hasten <hastenkamphuis@gmail.com>
2022-10-05 18:13:04 -0400

    Writing Dialogue

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -54,7 +54,7 @@ public class GameManager : MonoBehaviour
 
     public void AdvanceDialogue()
     {
-        if (phaseIndex > 4)
+        if (phaseIndex < 4)
         {
 
             dialogueIndex++;
