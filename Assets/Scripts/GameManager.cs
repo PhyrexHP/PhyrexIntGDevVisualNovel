0bec4ddaec32a8864603783ce9fa7f6230633851
Phyrex Hasten <hastenkamphuis@gmail.com>
2022-10-05 13:41:46 -0400

    Title Scene + Script Scaffold done

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -46,6 +46,94 @@ public class GameManager : MonoBehaviour
 
     void SetDialogueText()
     {
+        if (phaseIndex < 4)
+        {
+            dialogueBox.text = currentDialogue[dialogueIndex];
+        }
+    }
 
+    public void AdvanceDialogue()
+    {
+        if (phaseIndex > 4)
+        {
+
+            dialogueIndex++;
+            SetDialogueText();
+
+            if (dialogueIndex == currentDialogue.Count - 1)
+            {
+                SetupChoices();
+            }
+        }
+        else
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+
+    void SetupChoices()
+    {
+        nextButton.SetActive(false);
+        choiceOne.SetActive(false);
+        choiceTwo.SetActive(false);
+    }
+
+    public void NoChoice()
+    {
+        GoToNextPart();
+    }
+
+    public void YesChoice()
+    {
+        yesNumber++;
+        GoToNextPart();
+    }
+
+    void GoToNextPart()
+    {
+
+        nextButton.SetActive(true);
+        choiceOne.SetActive(false);
+        choiceTwo.SetActive(false);
+
+        dialogueIndex = 0;
+
+        switch (phaseIndex)
+        {
+            case 0:
+                phaseIndex = 1;
+                currentDialogue = partTwoDialogue;
+                break;
+            case 1:
+                phaseIndex = 2;
+                currentDialogue = partThreeDialogue;
+                break;
+            case 2:
+                phaseIndex = 3;
+                currentDialogue = partFourDialogue;
+                break;
+            case 3:
+                phaseIndex = 4;
+                break;
+            case 4:
+                phaseIndex = 5;
+                break;
+        }
+
+    SetDialogueText();
+
+    }
+
+    void GiveResults()
+    {
+
+        if (yesNumber > 3)
+        {
+            dialogueBox.text = endingOne;
+        }
+        else
+        {
+            dialogueBox.text = endingTwo;
+        }
     }
 }
